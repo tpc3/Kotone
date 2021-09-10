d9e77326031e3cba8df1e8ef3ce252e6fd13cc48
hayabusa2yk <public.yk@gmail.com>
2021-09-10 20:37:22 +0900

    Use code from original repo / Add cmudict build script

---
+++ cmudict.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -e "cmudict-0.7b_baseform" ]
+then
+    echo Baseform exists, skipping cmudict build...
+    exit 0
+fi
+curl http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/scripts/make_baseform.pl -o make_baseform.pl
+curl http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/scripts/cmudict-0.7b -o cmudict-0.7b
+perl make_baseform.pl cmudict-0.7b cmudict-0.7b_baseform
+rm make_baseform.pl cmudict-0.7b
